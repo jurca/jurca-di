9c1308aa97fdcfa76c544fed7f36a0588f8aacfb
Martin Jurča <mjurca@centrum.cz>
2015-09-21 20:47:38 +0200

    set up the testing environment

---
+++ test/DependencyInjectorSpec.js
@@ -0,0 +1,15 @@
+
+import DependencyInjector from "../es6/DependencyInjector"
+
+describe("DependencyInjector", () => {
+  let di
+
+  beforeEach(() => {
+    di = new DependencyInjector()
+  })
+
+  it("should work", () => {
+    // just assume that it does
+  })
+
+})
---
+++ test/main.js
@@ -0,0 +1,32 @@
+
+var allProjectFiles = [];
+var allTestFiles = [];
+var PROJECT_REGEXP = /es6\/.*\.js$/i;
+var TEST_REGEXP = /(spec|test)\.js$/i;
+
+var pathToModule = function(path) {
+  return path.replace(/^\/base\//, '').replace(/\.js$/, '');
+};
+
+Object.keys(window.__karma__.files).forEach(function (file) {
+  // load project and tests files and normalize paths to RequireJS module names
+  if (TEST_REGEXP.test(file)) {
+    allTestFiles.push(pathToModule(file));
+  } else if (PROJECT_REGEXP.test(file)) {
+    allProjectFiles.push(pathToModule(file));
+  }
+});
+
+require.config({
+  // Karma serves files under /base, which is the basePath from your config file
+  baseUrl: '/base',
+
+  paths: {
+  },
+
+  // dynamically load all test files
+  deps: allProjectFiles.concat(allTestFiles),
+
+  // we have to kickoff jasmine, as it is asynchronous
+  callback: window.__karma__.start
+});
